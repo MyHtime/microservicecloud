21423fb75314553a1badc90e747ed26e31668831
PN IN H HP <602827790@qq.com>
2019-06-15 00:44:16 +0800

    微服务提供者microservicecloud-provider-dept-8001

---
+++ microservicecloud-provider-dept-8001/src/main/java/cn/techpan/springcloud/mapper/DeptMapper.java
@@ -0,0 +1,22 @@
+package cn.techpan.springcloud.mapper;
+
+import cn.techpan.springcloud.entity.Dept;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface DeptMapper {
+
+    @Insert("insert into dept (#{dept_name}, #{database_source}) values (deptName, database())")
+    boolean addDept(Dept dept);
+
+    @Select("select * from dept where dept_no = #{deptNo}")
+    Dept findById(@Param("deptNo") Long id);
+
+    @Select("select * from dept")
+    List<Dept> findAll();
+}
